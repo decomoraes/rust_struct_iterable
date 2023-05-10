999ae4fde3436fc392f579645a3eb59bbc81b416
Deco Moraes <deco.moraes@icloud.com>
2023-05-10 13:29:42 -0300

    chore: remove target directory

--- struct_iterable_derive/target/debug/build/quote-b980b56f4db4b877/build_script_build-b980b56f4db4b877.d
+++
@@ -1,5 +0,0 @@
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/build/quote-b980b56f4db4b877/build_script_build-b980b56f4db4b877: /Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.27/build.rs
-
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/build/quote-b980b56f4db4b877/build_script_build-b980b56f4db4b877.d: /Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.27/build.rs
-
-/Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.27/build.rs:
--- struct_iterable_derive/target/debug/build/serde-5959c6fcda68bbfc/build_script_build-5959c6fcda68bbfc.d
+++
@@ -1,5 +0,0 @@
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/build/serde-5959c6fcda68bbfc/build_script_build-5959c6fcda68bbfc: /Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.162/build.rs
-
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/build/serde-5959c6fcda68bbfc/build_script_build-5959c6fcda68bbfc.d: /Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.162/build.rs
-
-/Users/decomoraes/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.162/build.rs:
