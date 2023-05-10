fb388991ade44dbc825591b5f5f4f0ec23c18262
Deco Moraes <deco.moraes@icloud.com>
2023-05-10 13:27:27 -0300

    chore: remove debug directory

--- struct_iterable_derive/target/debug/deps/struct_iterable_derive-7b6ca706ee88b6b3.d
+++
@@ -1,5 +0,0 @@
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/deps/struct_iterable_derive-7b6ca706ee88b6b3.rmeta: src/lib.rs
-
-/Users/decomoraes/Code/Rust/struct_iterable/struct_iterable_derive/target/debug/deps/struct_iterable_derive-7b6ca706ee88b6b3.d: src/lib.rs
-
-src/lib.rs:
