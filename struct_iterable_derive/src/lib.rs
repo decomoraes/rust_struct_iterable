e6e00529621993250d701ef15d65d8af19291afe
Michael House <mjhouse@protonmail.com>
2023-08-26 06:02:47 -0500

    Added iter_mut method to Iterable trait

--- struct_iterable_derive/src/lib.rs
+++ struct_iterable_derive/src/lib.rs
@@ -40,6 +40,22 @@ use struct_iterable_internal::Iterable;
 ///     println!("{}: {:?}", field_name, field_value);
 /// }
 /// ```
+///
+/// Or call the `iter_mut` method to modify the fields:
+///
+/// ```
+/// let mut my_instance = MyStruct {
+///     field1: 42,
+///     field2: "Hello, world!".to_string(),
+/// };
+///
+/// for (field_name, field_value) in my_instance.iter_mut() {
+///     if let Some(num) = field_value.downcast_mut::<i32>() {
+///         *num += 1;
+///     }
+/// }
+/// ```
+
 #[proc_macro_derive(Iterable)]
 pub fn derive_iterable(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -61,13 +77,29 @@ pub fn derive_iterable(input: TokenStream) -> TokenStream {
         }
     });
 
+    let fields_iter_mut = fields.iter().map(|field| {
+        let field_ident = &field.ident;
+        let field_name = field_ident.as_ref().unwrap().to_string();
+        quote! {
+            (#field_name, &mut (self.#field_ident) as &mut dyn std::any::Any)
+        }
+    });
+
     let expanded = quote! {
         impl Iterable for #struct_name {
+
             fn iter<'a>(&'a self) -> std::vec::IntoIter<(&'static str, &'a dyn std::any::Any)> {
                 vec![
                     #(#fields_iter),*
                 ].into_iter()
             }
+
+            fn iter_mut<'a>(&'a mut self) -> std::vec::IntoIter<(&'static str, &'a mut dyn std::any::Any)> {
+                vec![
+                    #(#fields_iter_mut),*
+                ].into_iter()
+            }
+
         }
     };
 
